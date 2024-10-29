70a8641b7b30591c801743ea8ce0a82d0c6bd354
Chau Phuc <chauphuc09090@gmail.com>
2024-10-29 13:51:40 +0700

    resolved merge conflict

--- src/Components/MenuBar/MenuList.jsx
+++ src/Components/MenuBar/MenuList.jsx
@@ -7,18 +7,18 @@ import {
   DollarOutlined,
 } from "@ant-design/icons";
 import "./MenuList.css";
-function MenuList() {
+function MenuList({userData}) {
   return (
     <Menu mode="inline" className="menu-bar">
       <Menu.Item key="home" icon={<HomeOutlined />}>
         <NavLink to="/">Home</NavLink>
       </Menu.Item>
-      <Menu.Item key="print" icon={<PrinterOutlined />}>
+      {!userData?.admin && <Menu.Item key="print" icon={<PrinterOutlined />}>
         <NavLink to="/print">Print</NavLink>
-      </Menu.Item>
-      <Menu.Item key="history" icon={<ClockCircleOutlined />}>
+      </Menu.Item>}
+      {!userData?.admin && <Menu.Item key="history" icon={<ClockCircleOutlined />}>
         <NavLink to="/history">History</NavLink>
-      </Menu.Item>
+      </Menu.Item>}
       <Menu.Item key="payment" icon={<DollarOutlined />}>
         <NavLink to="/payment">Payment</NavLink>
       </Menu.Item>
