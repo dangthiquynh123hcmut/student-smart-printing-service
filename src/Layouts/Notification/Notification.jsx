ec53f3db89acb7a11f8135f6c2be9d710e05a2a3
Dang Thi Quynh <quynh.dangthi2212880@hcmut.edu.vn>
2024-11-01 01:39:23 +0700

    update Admin home

--- src/Layouts/Notification/Notification.jsx
+++ src/Layouts/Notification/Notification.jsx
@@ -7,51 +7,51 @@ import moment from "moment";
 
 
 function Notification() {
-    const [notifications, setNotifications] = useState([]);
+  const [notifications, setNotifications] = useState([]);
 
-    useEffect(() => {
-        axios.get("https://6720b2f898bbb4d93ca593f3.mockapi.io/api/PrivateNotifications") // Đường dẫn đến API MockAPI
-            .then(response => setNotifications(response.data))
-            .catch(error => console.error("Error fetching notifications:", error));
-    }, []);
+  useEffect(() => {
+    axios.get("https://6720b2f898bbb4d93ca593f3.mockapi.io/api/PrivateNotifications") // Đường dẫn đến API MockAPI
+      .then(response => setNotifications(response.data))
+      .catch(error => console.error("Error fetching notifications:", error));
+  }, []);
 
-    return (
-        <div className="notifications-dropdown">
-            <h3>Thông báo</h3>
-            <hr className="custom-hr" />
-            <div className="notifications-list">
-            {notifications.length > 0 ? (
+  return (
+    <div className="notifications-dropdown">
+      <h3>Thông báo</h3>
+      <hr className="custom-hr" />
+      <div className="notifications-list">
+        {notifications.length > 0 ? (
 
-              
- notifications.map((notification) => (
-  <div className="notification1" key={notification.id}>
-    <div className="notification-header">
-      <img
-        src={notification.avatar}
-        alt="User Avatar"
-        className="notification-icon"
-      />
-      <div className="notification-header-content">
-        <span className="notification-name">{notification.name}</span>
-        <span className="notification-date">
-          {moment(notification.date).format("D/M/YYYY")}
-        </span>
+
+          notifications.map((notification) => (
+            <div className="notification1" key={notification.id}>
+              <div className="notification-header">
+                <img
+                  src={notification.avatar}
+                  alt="User Avatar"
+                  className="notification-icon"
+                />
+                <div className="notification-header-content">
+                  <span className="notification-name">{notification.name}</span>
+                  <span className="notification-date">
+                    {moment(notification.date).format("D/M/YYYY")}
+                  </span>
+                </div>
+              </div>
+              <div className="notification-body">
+                <h4 className="notification-title">{notification.title}</h4>
+                <p className="notification-text">{notification.content}</p>
+              </div>
+            </div>
+          ))
+
+        ) : (
+          <p>Loading new notifications...</p>
+        )}
+
       </div>
     </div>
-    <div className="notification-body">
-      <h4 className="notification-title">{notification.title}</h4>
-      <p className="notification-text">{notification.content}</p>
-    </div>
-  </div>
-))
 
-) : (
-  <p>Loading new notifications...</p>
-)}
-
-            </div>
-        </div>
-
-    );
+  );
 }
 export default Notification;
